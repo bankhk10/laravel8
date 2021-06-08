6feec8f3b737b115d3aa7288208e9f7172898f6b
bankhk10 <love48170@gmail.com>
2021-06-08 13:25:57 +0700

    add posts

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+@extends('master')
+
+@section('meta')
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="author" content="bank">
+    <title>Document</title>
+@endsection
+
+@section('content')
+    <h1>hi {{$name}}</h1>
+    <p>how to bank</p>
+
+
+@endsection
